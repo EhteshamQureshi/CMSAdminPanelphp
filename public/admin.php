4502f0119880ac49bed30a218c7b7b81f8277fa8
Ehtesham Qureshi <43238505+EhteshamQureshi@users.noreply.github.com>
2020-07-27 11:08:27 +0500

    Simple changes using visual studio checking update

---
+++ public/admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+
+<body>
+    <header id="header">
+        <h1>Widget Corp</h1>
+    </header>
+    <main>
+        <nav id="navigation">
+            &nbsp;
+        </nav>
+        <div id="page">
+            <h2> Admin Menu</h2>
+            <p> Welcome to the admin area</p>
+            <ul>
+                <li><a href="manage_content.php">Manage Website Content</a></li>
+                <li><a href="manage_admin.php">Manage Admin Users</a></li>
+                <li><a href="logout.php">Logout </a></li>
+            </ul>
+        </div>
+    </main>
+    <footer id="footer">Copyright 2020,Widget Corp</footer>
+</body>
+
+</html>
